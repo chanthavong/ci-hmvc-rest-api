d9722a0cffa2ac4388e696246198a34ebf9f2aca
Mark AI <chanthavong_12@hotmail.com>
2021-01-30 13:05:42 +0700

    Update jwt.php

--- application/config/jwt.php
+++ application/config/jwt.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['jwt_key'] = 'ingDLMRuGe9UKHRNjs7cYckS2yul4lc3';
+$config['jwt_key'] = 'ingDLMRuGe9UKHRNjs7cYckS2yul4lc3retfcgdfgxcvgfdhjtyhjbvj';
 
 /*Generated token will expire in 1 minute for sample code
 * Increase this value as per requirement for production
